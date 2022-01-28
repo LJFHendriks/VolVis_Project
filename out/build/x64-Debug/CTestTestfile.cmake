b2e327c84829ff42a8881fbfc279d98e54550722
LJF Hendriks <leo.hendriks@live.nl>
2022-01-28 13:26:50 +0100

    Adding Project files and progress from Alin

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Code/Study/VolVis_Git/scivis_project
+# Build directory: D:/Code/Study/VolVis_Git/scivis_project/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("integrity_tests")
---
+++ out/build/x64-Debug/integrity_tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Code/Study/VolVis_Git/scivis_project/integrity_tests
+# Build directory: D:/Code/Study/VolVis_Git/scivis_project/out/build/x64-Debug/integrity_tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
